d3c7aef28e0e939d16d79c1953b1d4bd576badc8
Crispin Andrade <crispin.andrade95@gmail.com>
2020-11-24 21:16:25 +1100

    Basic  loops and types

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,6 +7,17 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var numbers = new[] { 55.23, 532.643, 32.53 };
+            List<double> grades = new List<double>() { 55.23, 532.643, 32.53 };
+
+            var result = 0.0;
+            foreach(var n in grades) {
+                result += n;
+            }
+
+            var average = result / grades.Count;
+            Console.WriteLine($"The average grade is {average:n2}");
+
             if (args.Length > 0) {
                 Console.WriteLine($"Hello {args[0]}!");
             }
